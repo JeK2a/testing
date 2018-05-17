54edd7e58167672ecafb9d36ebbb07039bf44cf3
JeK2a <31507511+JeK2a@users.noreply.github.com>
2018-05-18 00:57:06 +0300

    Первоначальная заливка.

---
+++ js/script.js
@@ -0,0 +1,57 @@
+var element = document.body.getElementsBy ClassName('d'); // children[1];
+
+element.style.cssText="color: green !important; \
+    background-color: yellow; \
+    width: 100px; \
+    text-align: center; \
+    blabla: 5; \
+  ";
+
+// alert(element.style.cssText);
+
+// onload = function(){
+//     var obj;
+//     obj = document.getElementsByClassName(".c");
+//     obj.onmouseover=function(){
+//         // this.value='size=3';
+//         this.style.color = 'red';
+//     };
+//     obj.onmouseout=function(){
+//         // this.value='size=7';
+//         this.style.color = 'blue';
+//     };
+// };
+
+// document.event
+// document.getElementsByClassName(".c").style.color = "#06ff00";
+
+// document.getElementsByClassName(".c").style.color = "#06ff00";
+
+// setColorText(document.getElementsByClassName('.c'));
+
+// function setColorText(element) {
+//     element.style.color = "#06ff00";
+// }
+
+// $(document).ready(function() {
+
+    // document.getElementsByClassName("c").style.color = "#06ff00";
+    // $('.c').style.textContent.fontcolor("green");
+    // $('.c').style.color = "green";
+
+    // $('.c').css('color', 'green');
+
+    // $('.c').each(function () {
+    //     $(this).css("color", "green")
+    // });
+
+// });
+
+
+// $(document).ready(function(){
+//
+//     $('.c').style.background = "black";
+//     $('.c').style.color = "green";
+//     $('.c').css('background-color', 'green');
+// });
+// document.getElementsByClassName(".c").style.color = "green";
